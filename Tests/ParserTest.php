d38918c18e24a29bd9822745d82693fb10d1da01
Francesco Montefoschi <francesco.monte@gmail.com>
2010-11-17 21:55:52 +0100

    moved sample classes into fixture folder

--- Tests/ParserTest.php
+++ Tests/ParserTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once '../PHPADD/Parser.php';
+require_once 'fixtures/sample.classes';
 
 class ParserTest extends PHPUnit_Framework_TestCase
 {
@@ -54,46 +55,4 @@ class ParserTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(1, count($analysys));
 		$this->assertEquals('unexpected-param', $analysys[0]['detail'][0]['type']);
 	}
-}
-
-class Example
-{
-	public function publicMethod() {}
-	protected function protectedMethod() {}
-	private function privateMethod() {}
-}
-
-class ValidExample
-{
-	/**
-	 * Some description here
-	 * 
-	 * @param stdClass $my
-	 * @param string $name
-	 * @return string
-	 */
-	public function validMethod(stdClass $my, $name) {}
-}
-
-class InvalidMissingExample
-{
-	/**
-	 * Some description here
-	 *
-	 * @param stdClass $my
-	 * @return string
-	 */
-	public function validMethod(stdClass $my, $name) {}
-}
-
-class InvalidRemovedExample
-{
-	/**
-	 * Some description here
-	 *
-	 * @param stdClass $my
-	 * @param string $name
-	 * @return string
-	 */
-	public function validMethod(stdClass $my) {}
 }
