2c3c743c5c9307341bf34a0a72cb5c84059f8fd9
Francesco Montefoschi <francesco.monte@gmail.com>
2010-11-28 14:38:36 +0100

    removed useless tests

--- Tests/CliTest.php
+++ Tests/CliTest.php
@@ -14,26 +14,6 @@ class CliTest extends PHPUnit_Framework_TestCase
 		$this->assertFalse($this->cli->blocksPrivate());
 		$this->assertFalse($this->cli->blocksProtected());
 	}
-
-	public function testBlockPrivatesByParam()
-	{
-		$_SERVER['argv'] = array(
-			'script', '--anyparam', '--skip-private', '--any-other-param'
-		);
-
-		$this->assertTrue($this->cli->blocksPrivate());
-		$this->assertFalse($this->cli->blocksProtected());
-	}
-
-	public function testBlockProtectedByParam()
-	{
-		$_SERVER['argv'] = array(
-			'script', '--anyparam', '--skip-protected', '--any-other-param'
-		);
-
-		$this->assertFalse($this->cli->blocksPrivate());
-		$this->assertTrue($this->cli->blocksProtected());
-	}
 }
 
 class Unmasked_PHPADD_Cli extends PHPADD_Cli
