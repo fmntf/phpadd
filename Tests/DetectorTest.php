143902746e2f4174d6fc04016c45a512c428938b
Francesco Montefoschi <francesco.monte@gmail.com>
2010-12-21 13:02:06 +0100

    fixed detector tests

--- Tests/DetectorTest.php
+++ Tests/DetectorTest.php
@@ -12,73 +12,100 @@ class DetectorTest extends PHPUnit_Framework_TestCase
 
 	public function testDelegatesanalysisToParser()
 	{
-		$this->markTestSkipped('This test needs to be recreated');
-		
-		$filter = new PHPADD_Filter();
-		$detector = $this->getMockBuilder('PHPADD_Parser')
+		$detector = $this->getMockBuilder('Unmasked_Detector')
 						->disableOriginalConstructor()
 						->getMock();
 		$detector->expects($this->once())
 				->method('analyze')
-				->with($filter)
+				->with('myClass')
 				->will($this->returnValue('delegated'));
 
-		$this->assertEquals('delegated', $detector->analyze($filter));
+		$this->assertEquals('delegated', $detector->analyze('myClass'));
 	}
 
 	public function testWillSkipCleanClasses()
 	{
 		$mess = $this->detector->getMess(__DIR__ . '/fixtures/clean');
+
 		$files = $mess->getFiles();
 		$this->assertEquals(1, $mess->getCount());
 
 		$classes = $files[0]->getClasses();
-		$this->assertEquals(1, $mess->getCount());
+		$validClasses = array('Fixture_ValidExample', 'Fixture_ValidWithSpacesExample');
 
-		$this->assertEquals(0, count($classes[0]->getMethods()));
-		$this->assertEquals(0, count($classes[0]->getMissingBlocks()));
-		$this->assertEquals(0, count($classes[0]->getOutdatedBlocks()));
-		$this->assertEquals(1, count($classes[0]->getRegularBlocks()));
+		foreach ($validClasses as $validClass) {
+			$this->assertEquals(0, count($classes[$validClass]->getMethods()));
+			$this->assertEquals(0, count($classes[$validClass]->getMissingBlocks()));
+			$this->assertEquals(0, count($classes[$validClass]->getOutdatedBlocks()));
+			$this->assertEquals(1, count($classes[$validClass]->getRegularBlocks()));
 
-		$this->assertTrue($classes[0]->isClean());
+			$this->assertTrue($classes[$validClass]->isClean());
+		}
 	}
 
-	public function testWillReportDirtyClasses()
+	public function testReportsDirtyClasses()
 	{
-		$this->markTestSkipped('This test needs to be recreated');
-		
 		$mess = $this->detector->getMess(__DIR__ . '/fixtures/dirty');
-		$results = $mess->getResults();
 
-		$missingParam = $results['Fixture_InvalidMissingExample'];
-		$removedParam = $results['Fixture_InvalidRemovedExample'];
+		$this->assertEquals(1, $mess->getCount());
+		$files = $mess->getFiles();
+		$file = $files[0];
+
+		$this->assertEquals(4, $file->getCount());
+		$classes = $file->getClasses();
+
+		$missingParam = $classes['Fixture_InvalidMissingExample'];
+		$removedParam = $classes['Fixture_InvalidRemovedExample'];
+		$multi = $classes['Fixture_InvalidMultiExample'];
+		$noblock = $classes['Fixture_NodobBLock']; // @todo
 
 		// nothing regular
 		$this->assertEquals(0, $missingParam->getRegularBlocks());
 		$this->assertEquals(0, $removedParam->getRegularBlocks());
+		$this->assertEquals(0, $multi->getRegularBlocks());
+		$this->assertEquals(0, $noblock->getRegularBlocks());
 
-		// nothing without docblock
+		// only last class miss docblock
 		$this->assertEquals(0, count($missingParam->getMissingBlocks()));
 		$this->assertEquals(0, count($removedParam->getMissingBlocks()));
+		$this->assertEquals(0, count($multi->getMissingBlocks()));
+		$this->assertEquals(1, count($noblock->getMissingBlocks()));
 
 		//just one warning
 		$this->assertEquals(1, count($missingParam->getOutdatedBlocks()));
 		$this->assertEquals(1, count($removedParam->getOutdatedBlocks()));
+		$this->assertEquals(1, count($multi->getOutdatedBlocks()));
+		$this->assertEquals(0, count($noblock->getOutdatedBlocks()));
+	}
 
-		$missingParamOutdates = $missingParam->getOutdatedBlocks();
-		$removedParamOutdates = $removedParam->getOutdatedBlocks();
+	public function testReportsMissingParams()
+	{
+		$mess = $this->detector->getMess(__DIR__ . '/fixtures/dirty');
 
-		$this->assertEquals(1, count($missingParamOutdates));
-		$this->assertEquals(1, count($removedParamOutdates));
+		$files = $mess->getFiles();
+		$classes = $files[0]->getClasses();
+		$outdates = $classes['Fixture_InvalidMissingExample']->getOutdatedBlocks();
 
-		$missingParamDetail = $missingParamOutdates[0]->getDetail();
-		$removedParamDetail = $removedParamOutdates[0]->getDetail();
+		$this->assertEquals('invalidMethod', $outdates[0]->getName());
+		$detail = $outdates[0]->getDetail();
+		$expectedDetail = array('type' => 'missing-param', 'name' => '$name');
+		$this->assertEquals(1, count($detail));
+		$this->assertEquals($expectedDetail, $detail[0]);
+	}
 
-		$this->assertEquals(1, count($missingParamOutdates));
-		$this->assertEquals(1, count($removedParamOutdates));
+	public function testReportsUnexpectedParams()
+	{
+		$mess = $this->detector->getMess(__DIR__ . '/fixtures/dirty');
 
-		$this->assertEquals('missing-param', $missingParamDetail[0]['type']);
-		$this->assertEquals('unexpected-param', $removedParamDetail[0]['type']);
+		$files = $mess->getFiles();
+		$classes = $files[0]->getClasses();
+		$outdates = $classes['Fixture_InvalidRemovedExample']->getOutdatedBlocks();
+
+		$this->assertEquals('invalidMethod', $outdates[0]->getName());
+		$detail = $outdates[0]->getDetail();
+		$expectedDetail = array('type' => 'unexpected-param', 'name' => '$name');
+		$this->assertEquals(1, count($detail));
+		$this->assertEquals($expectedDetail, $detail[0]);
 	}
 }
 
