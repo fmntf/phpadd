99c6e43e08acddb379f96d39fce286f8cacd8178
Francesco Montefoschi <francesco.monte@gmail.com>
2011-02-23 19:04:23 +0100

    Added a bootstrap as example which defines a simple autoloader

---
+++ bootstrapexample.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Auto-loads a class file. Do not call this directly!
+ *
+ * @param string $class
+ */
+function autoloader($class)
+{
+	$bases = array(
+		'/home/francesco/project/library/',
+	);
+
+	foreach ($bases as $base)
+	{
+		$path = $base . str_replace('_', '/', $class) . '.php';
+
+		if (is_file($path)) {
+			require_once $path;
+			return true;
+		}
+	}
+}
+
+spl_autoload_register('autoloader');
