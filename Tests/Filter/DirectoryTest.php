3ba50678936e71cf60df430a472bf9107db8b750
Francesco Montefoschi <francesco.monte@gmail.com>
2011-03-03 22:47:45 +0100

    Created Class, Directory and Method (basic) filters

---
+++ Tests/Filter/DirectoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class Filter_DirectoryTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @dataProvider getDirectories
+	 */
+	public function testSkipsDirectories($directory, $result)
+	{
+		// Foo is the regular expression to discard
+		$filter = new PHPADD_Filter_Directory(array('Foo'));
+		
+		$this->assertSame($result, $filter->isFiltered($directory));
+	}
+	
+	public function getDirectories()
+	{
+		return array(
+			array('Foo',		true),
+			array('FooBar',		true),
+			array('Bar',		false),
+			array('Foo/Bar',	true),
+			array('Baz/Bar',	false)
+		);
+	}
+}
